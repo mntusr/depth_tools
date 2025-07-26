Fix nan gradients at normalize_values

src/depth_tools/_diverging_functions_internal.py
@@ -3,14 +3,20 @@ from typing import Any
 import numpy as np
 
 
-def nanmedian(a: np.ndarray, along_all_dims_except_0: bool) -> np.ndarray:
+def masked_median_unchecked(
+    a: np.ndarray, mask: np.ndarray, along_all_dims_except_0: bool
+) -> np.ndarray:
     """
-    A nanmedian function that works similarly for both Numpy and Pytorch.
+    A function that calculates the median for the array elements selected by a given mask.
+
+    This function does not check its arguments.
 
     Parameters
     ----------
     a
-        The array on which the nanmedian calculation should be done.
+        The array on which the median calculation should be done. Format: any floating point array
+    mask
+        The array that selects the elements used to calculate the median. Format: its shape is the same as the shape of ``a``, its dtype is bool
     along_all_dims_except_0
         If true, then the median calculation is done along all dimensions, except 0. Otherwise, the median calculation is done alongside all dimensions.
 
@@ -19,23 +25,32 @@ def nanmedian(a: np.ndarray, along_all_dims_except_0: bool) -> np.ndarray:
     v
         The calculated median.
     """
+
+    a = a.copy()
+    a[~mask] = np.nan
     if along_all_dims_except_0:
-        a_reshaped = a.reshape(len(a), -1, order="C")
+        a_reshaped = a.reshape(len(a), -1)
         nanmedian_values = np.nanmedian(a_reshaped, axis=1)
         new_shape: list[int] = [len(nanmedian_values)] + [1] * (len(a.shape) - 1)
-        return np.reshape(nanmedian_values, new_shape, order="C")
+        return np.reshape(nanmedian_values, new_shape)
     else:
         return np.array(np.nanmedian(a))
 
 
-def nanmean(a: np.ndarray, along_all_dims_except_0: bool) -> np.ndarray:
+def masked_mean_unchecked(
+    a: np.ndarray, mask: np.ndarray, along_all_dims_except_0: bool
+) -> np.ndarray:
     """
-    A nanmean function that works similarly for both Numpy and Pytorch
+    A function that calculates the mean for the array elements selected by a given mask.
+
+    This function does not check its arguments.
 
     Parameters
     ----------
     a
-        The array on which the nanmedian calculation should be done.
+        The array on which the mean calculation should be done. Format: any floating point array
+    mask
+        The array that selects the elements used to calculate the median. Format: its shape is the same as the shape of ``a``, its dtype is bool
     along_all_dims_except_0
         If true, then the mean calculation is done along all dimensions, except 0. Otherwise, the mean calculation is done alongside all dimensions.
 
@@ -44,11 +59,15 @@ def nanmean(a: np.ndarray, along_all_dims_except_0: bool) -> np.ndarray:
     v
         The calculated median.
     """
+
+    a = a.copy()
+    a[~mask] = np.nan
+
     if along_all_dims_except_0:
-        a_reshaped = a.reshape(len(a), -1, order="C")
+        a_reshaped = a.reshape(len(a), -1)
         nanmean_values = np.nanmean(a_reshaped, axis=1)
         new_shape: list[int] = [len(nanmean_values)] + [1] * (len(a.shape) - 1)
-        return np.reshape(nanmean_values, new_shape, order="C")
+        return np.reshape(nanmean_values, new_shape)
     else:
         return np.array(np.nanmean(a))
 

src/depth_tools/_normalize_values_univ.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-from ._diverging_functions_internal import nanmean, nanmedian
+from ._diverging_functions_internal import (
+    masked_mean_unchecked,
+    masked_median_unchecked,
+)
 from ._format_checks_internal import is_bool_array, is_dim_sel_ok, is_floating_array
 
 
@@ -35,13 +38,12 @@ def normalize_values(
             values=values, mask=mask, first_dim_separates=first_dim_separates
         )
 
-    values = values.copy()
+    values_median = masked_median_unchecked(
+        values, mask=mask, along_all_dims_except_0=first_dim_separates
+    )
 
-    values[~mask] = np.nan
-    values_median = nanmedian(values, along_all_dims_except_0=first_dim_separates)
-
-    normalized_pred = (values - values_median) / nanmean(
-        (values - values_median), along_all_dims_except_0=first_dim_separates
+    normalized_pred = (values - values_median) / masked_mean_unchecked(
+        (values - values_median), mask=mask, along_all_dims_except_0=first_dim_separates
     )
     normalized_pred[~mask] = 0
 

src/depth_tools/pt/_diverging_functions_internal.py
@@ -3,14 +3,20 @@ from typing import Any
 import torch
 
 
-def nanmedian(a: torch.Tensor, along_all_dims_except_0: bool) -> torch.Tensor:
+def masked_median_unchecked(
+    a: torch.Tensor, mask: torch.Tensor, along_all_dims_except_0: bool
+) -> torch.Tensor:
     """
-    A nanmedian function that works similarly for both Numpy and Pytorch.
+    A function that calculates the median for the array elements selected by a given mask.
+
+    This function does not check its arguments.
 
     Parameters
     ----------
     a
-        The array on which the nanmedian calculation should be done.
+        The array on which the median calculation should be done. Format: any floating point array
+    mask
+        The array that selects the elements used to calculate the median. Format: its shape is the same as the shape of ``a``, its dtype is bool
     along_all_dims_except_0
         If true, then the median calculation is done along all dimensions, except 0. Otherwise, the median calculation is done alongside all dimensions.
 
@@ -19,6 +25,10 @@ def nanmedian(a: torch.Tensor, along_all_dims_except_0: bool) -> torch.Tensor:
     v
         The calculated median.
     """
+
+    a = a.clone()
+    a[~mask] = torch.nan
+
     if along_all_dims_except_0:
         a_reshaped = a.reshape(len(a), -1)
         nanmedian_values = torch.nanmedian(a_reshaped, dim=1).values
@@ -28,14 +38,20 @@ def nanmedian(a: torch.Tensor, along_all_dims_except_0: bool) -> torch.Tensor:
         return torch.nanmedian(a)
 
 
-def nanmean(a: torch.Tensor, along_all_dims_except_0: bool) -> torch.Tensor:
+def masked_mean_unchecked(
+    a: torch.Tensor, mask: torch.Tensor, along_all_dims_except_0: bool
+) -> torch.Tensor:
     """
-    A nanmean function that works similarly for both Numpy and Pytorch
+    A function that calculates the mean for the array elements selected by a given mask.
+
+    This function does not check its arguments.
 
     Parameters
     ----------
     a
-        The array on which the nanmedian calculation should be done.
+        The array on which the mean calculation should be done. Format: any floating point array
+    mask
+        The array that selects the elements used to calculate the median. Format: its shape is the same as the shape of ``a``, its dtype is bool
     along_all_dims_except_0
         If true, then the mean calculation is done along all dimensions, except 0. Otherwise, the mean calculation is done alongside all dimensions.
 
@@ -44,6 +60,10 @@ def nanmean(a: torch.Tensor, along_all_dims_except_0: bool) -> torch.Tensor:
     v
         The calculated median.
     """
+
+    a = a.clone()
+    a[~mask] = torch.nan
+
     if along_all_dims_except_0:
         a_reshaped = a.reshape(len(a), -1)
         nanmean_values = torch.nanmean(a_reshaped, dim=1)

test/test_nomralize_values.py
@@ -137,26 +137,51 @@ class TestNormalizeValues(npy_unittest.NpyTestCase):
         self.assertIn(str(self.original_values.size), msg)
 
     def test_normalize_values__happy_path__multiple_pt(self):
+        values = torch.from_numpy(self.original_values).requires_grad_(True)
+
         actual_normalized_values = depth_tools.pt.normalize_values(
-            values=torch.from_numpy(self.original_values),
+            values=values,
             mask=torch.from_numpy(self.mask),
             verify_args=True,
             first_dim_separates=True,
-        ).numpy()
+        )
 
-        self.assertAllclose(self.expected_normalized_values, actual_normalized_values)
+        self.assertAllclose(
+            self.expected_normalized_values, actual_normalized_values.detach().numpy()
+        )
+
+        actual_normalized_values.mean().backward()
+
+        # normalize_values internally uses nan to represent non-selected values,
+        # so we have to be extra sure that these nans do not leak into the gradients
+        grad = values.grad
+        assert grad is not None
+        grad_has_nan = torch.any(torch.isnan(grad))
+        self.assertFalse(grad_has_nan)
 
     def test_normalize_values__happy_path__single_pt(self):
+        values = torch.from_numpy(self.original_values[0]).requires_grad_(True)
         actual_normalized_values = depth_tools.pt.normalize_values(
-            values=torch.from_numpy(self.original_values[0]),
+            values=values,
             mask=torch.from_numpy(self.mask[0]),
             verify_args=True,
             first_dim_separates=True,
-        ).numpy()
+        )
 
         self.assertAllclose(
-            self.expected_normalized_values[0], actual_normalized_values, atol=1e-4
+            self.expected_normalized_values[0],
+            actual_normalized_values.detach().numpy(),
+            atol=1e-4,
         )
+
+        actual_normalized_values.mean().backward()
+
+        # normalize_values internally uses nan to represent non-selected values,
+        # so we have to be extra sure that these nans do not leak into the gradients
+        grad = values.grad
+        assert grad is not None
+        grad_has_nan = torch.any(torch.isnan(grad))
+        self.assertFalse(grad_has_nan)
 
     def test_normalize_values__single_value_pt(self):
         with self.assertRaises(ValueError) as cm: