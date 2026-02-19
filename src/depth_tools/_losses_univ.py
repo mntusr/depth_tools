Fix mean calculation in the losses

src/depth_tools/_losses_univ.py
@@ -2,6 +2,7 @@ from typing import Any, Protocol
 
 import numpy as np
 
+from ._diverging_functions_internal import masked_mean_unchecked
 from ._format_checks_internal import is_bool_array, is_floating_array
 
 
@@ -64,9 +65,14 @@ def dx_loss(
     loss_vals: np.ndarray = deltas < (1.25**x)
     loss_vals = loss_vals.astype(pred.dtype)
 
-    return _calculate_masked_mean_unchecked(
-        values=loss_vals, mask=mask, first_dim_separates=first_dim_separates
+    masked_mean = masked_mean_unchecked(
+        a=loss_vals, mask=mask, along_all_dims_except_0=first_dim_separates
     )
+
+    if not first_dim_separates:
+        masked_mean = np.expand_dims(masked_mean, axis=0)
+
+    return masked_mean
 
 
 def mse_loss(
@@ -109,9 +115,14 @@ def mse_loss(
 
     x = (pred - gt) ** 2
 
-    return _calculate_masked_mean_unchecked(
-        values=x, mask=mask, first_dim_separates=first_dim_separates
+    masked_mean = masked_mean_unchecked(
+        a=x, mask=mask, along_all_dims_except_0=first_dim_separates
     )
+
+    if not first_dim_separates:
+        masked_mean = np.expand_dims(masked_mean, axis=0)
+
+    return masked_mean
 
 
 def mse_log_loss(
@@ -152,9 +163,14 @@ def mse_log_loss(
         )
     x = (np.log(pred) - np.log(gt)) ** 2
 
-    return _calculate_masked_mean_unchecked(
-        values=x, mask=mask, first_dim_separates=first_dim_separates
+    masked_mean = masked_mean_unchecked(
+        a=x, mask=mask, along_all_dims_except_0=first_dim_separates
     )
+
+    if not first_dim_separates:
+        masked_mean = np.expand_dims(masked_mean, axis=0)
+
+    return masked_mean
 
 
 class EvalBuilder:
@@ -384,34 +400,3 @@ def _verify_loss_args(
         raise ValueError(
             f"The prediction array should be at least two dimensional if the first dimension separates the samples. The current shape of the prediction array: {tuple(pred.shape)}"
         )
-
-
-def _calculate_masked_mean_unchecked(
-    values: np.ndarray,
-    mask: np.ndarray,
-    first_dim_separates: bool = False,
-) -> np.ndarray:
-    """
-    A function that calculates the masked mean of the given values.
-
-    This function does not check its arguments.
-
-    Parameters
-    ----------
-    values
-        The values of which the mean should be calculated. Format: the array should contain floating data. If ``first_dim_separates``, then it should have at least two dimensions. It should have the same shape as the predicted values.
-    mask
-        The masks that select the relevant values. Format: the array should contain boolean data. If ``first_dim_separates``, then it should have at least two dimensions. It should have the same shape as the predicted values.
-    first_dim_separates
-        If this is true, then the mean calculation is done for each element along dimension 0 individually. Otherwise the claculation is done for the whole array globally.
-    """
-
-    if first_dim_separates:
-        dim = tuple(range(1, len(values.shape)))
-    else:
-        dim = None
-
-    values = values * mask
-    return values.mean(axis=dim) * (
-        np.ones_like(values).sum(axis=dim) / mask.astype(values.dtype).sum(axis=dim)
-    )

src/depth_tools/pt/_diverging_functions_internal.py
@@ -4,7 +4,10 @@ import torch
 
 
 def masked_median_unchecked(
-    a: torch.Tensor, mask: torch.Tensor, along_all_dims_except_0: bool
+    a: torch.Tensor,
+    mask: torch.Tensor,
+    along_all_dims_except_0: bool,
+    keep_dropped_dims: bool = False,
 ) -> torch.Tensor:
     """
     A function that calculates the median for the array elements selected by a given mask.
@@ -19,27 +22,56 @@ def masked_median_unchecked(
         The array that selects the elements used to calculate the median. Format: its shape is the same as the shape of ``a``, its dtype is bool
     along_all_dims_except_0
         If true, then the median calculation is done along all dimensions, except 0. Otherwise, the median calculation is done alongside all dimensions.
+    keep_dropped_dims
+        Same as the ``keepdim`` argument of the similar funcitons.
 
     Returns
     -------
     v
-        The calculated median.
+        The calculated median. Format:
+
+        - a single 0-dimensional scalar if ``along_all_dims_except_0=False`` and ``keep_dropped_dims=False``
+        - a one-dimensional array if ``along_all_dims_except_0=True`` and ``keep_dropped_dims=False``, where each element denotes the corresponding median
+        - an array with the same number of dimensions as ``a`` where dimension 0 denotes the index of the element, the other dimensions have length 1, if ``along_all_dims_except_0=<any>`` and ``keep_dropped_dims=True``, where each element denotes the corresponding median
     """
 
-    a = a.clone()
-    a[~mask] = torch.nan
-
     if along_all_dims_except_0:
-        a_reshaped = a.reshape(len(a), -1)
-        nanmedian_values = torch.nanmedian(a_reshaped, dim=1).values
-        new_shape: list[int] = [len(nanmedian_values)] + [1] * (len(a.shape) - 1)
-        return torch.reshape(nanmedian_values, new_shape)
+        mean_val_list: list[torch.Tensor] = []
+
+        # For loops are generally not efficient.
+        # However in this case, I was not able to come up
+        # with any other idea that plays nicely with nans and
+        # simple to implement.
+        #
+        # The first dimension is typically not that large (1-32) anyway
+        # (at least for the cases where this function is used).
+        for i in range(len(a)):
+            mean_val = torch.median(a[i][mask[i]])
+            mean_val_list.append(mean_val)
+        mean_val_arr = torch.stack(mean_val_list)
+        if keep_dropped_dims:
+            new_shape: list[int] = [len(mean_val_arr)] + [1] * (len(a.shape) - 1)
+            return torch.reshape(mean_val_arr, new_shape)
+        else:
+            return mean_val_arr
     else:
-        return torch.nanmedian(a)
+        a = a.clone()
+        a[~mask] = torch.nan
+        nan_canary = a[mask].min() * 0
+        nanmed_arr = torch.nanmedian(a)
+
+        if keep_dropped_dims:
+            new_shape = [1] * len(a.shape)
+            return torch.reshape(nanmed_arr, new_shape) + nan_canary
+        else:
+            return nanmed_arr + nan_canary
 
 
 def masked_mean_unchecked(
-    a: torch.Tensor, mask: torch.Tensor, along_all_dims_except_0: bool
+    a: torch.Tensor,
+    mask: torch.Tensor,
+    along_all_dims_except_0: bool,
+    keep_dropped_dims: bool = False,
 ) -> torch.Tensor:
     """
     A function that calculates the mean for the array elements selected by a given mask.
@@ -54,23 +86,49 @@ def masked_mean_unchecked(
         The array that selects the elements used to calculate the median. Format: its shape is the same as the shape of ``a``, its dtype is bool
     along_all_dims_except_0
         If true, then the mean calculation is done along all dimensions, except 0. Otherwise, the mean calculation is done alongside all dimensions.
+    keep_dropped_dims
+        Same as the ``keepdim`` argument of the similar funcitons.
 
     Returns
     -------
     v
-        The calculated median.
+        The calculated median. Format:
+
+        - a single 0-dimensional scalar if ``along_all_dims_except_0=False`` and ``keep_dropped_dims=False``
+        - a one-dimensional array if ``along_all_dims_except_0=True`` and ``keep_dropped_dims=False``, where each element denotes the corresponding mean
+        - an array with the same number of dimensions as ``a`` where dimension 0 denotes the index of the element, the other dimensions have length 1, if ``along_all_dims_except_0=<any>`` and ``keep_dropped_dims=True``, where each element denotes the corresponding mean
     """
 
-    a = a.clone()
-    a[~mask] = torch.nan
-
     if along_all_dims_except_0:
-        a_reshaped = a.reshape(len(a), -1)
-        nanmean_values = torch.nanmean(a_reshaped, dim=1)
-        new_shape: list[int] = [len(nanmean_values)] + [1] * (len(a.shape) - 1)
-        return torch.reshape(nanmean_values, new_shape)
+        mean_val_list: list[torch.Tensor] = []
+
+        # For loops are generally not efficient.
+        # However in this case, I was not able to come up
+        # with any other idea that plays nicely with nans and
+        # simple to implement.
+        #
+        # The first dimension is typically not that large (1-32) anyway
+        # (at least for the cases where this function is used).
+        for i in range(len(a)):
+            mean_val = torch.mean(a[i][mask[i]])
+            mean_val_list.append(mean_val)
+        mean_val_arr = torch.stack(mean_val_list)
+        if keep_dropped_dims:
+            new_shape: list[int] = [len(mean_val_arr)] + [1] * (len(a.shape) - 1)
+            return torch.reshape(mean_val_arr, new_shape)
+        else:
+            return mean_val_arr
     else:
-        return torch.nanmean(a)
+        a = a.clone()
+        a[~mask] = torch.nan
+        nan_canary = a[mask].min() * 0
+        nanmean_arr = torch.nanmean(a)
+
+        if keep_dropped_dims:
+            new_shape = [1] * len(a.shape)
+            return torch.reshape(nanmean_arr, new_shape) + nan_canary
+        else:
+            return nanmean_arr + nan_canary
 
 
 def new_full(

src/depth_tools/pt/_losses_univ.py
@@ -3,6 +3,7 @@ from typing import Any, Protocol
 
 import torch
 
+from ._diverging_functions_internal import masked_mean_unchecked
 from ._format_checks_internal import is_bool_array, is_floating_array
 
 
@@ -65,8 +66,10 @@ def dx_loss(
     loss_vals: torch.Tensor = deltas < (1.25**x)
     loss_vals = loss_vals.to(pred.dtype)
 
-    return _calculate_masked_mean_unchecked(
-        values=loss_vals, mask=mask, first_dim_separates=first_dim_separates
+    return masked_mean_unchecked(
+        a=loss_vals,
+        mask=mask,
+        along_all_dims_except_0=first_dim_separates,
     )
 
 
@@ -110,8 +113,8 @@ def mse_loss(
 
     x = (pred - gt) ** 2
 
-    return _calculate_masked_mean_unchecked(
-        values=x, mask=mask, first_dim_separates=first_dim_separates
+    return masked_mean_unchecked(
+        a=x, mask=mask, along_all_dims_except_0=first_dim_separates
     )
 
 
@@ -153,8 +156,8 @@ def mse_log_loss(
         )
     x = (torch.log(pred) - torch.log(gt)) ** 2
 
-    return _calculate_masked_mean_unchecked(
-        values=x, mask=mask, first_dim_separates=first_dim_separates
+    return masked_mean_unchecked(
+        a=x, mask=mask, along_all_dims_except_0=first_dim_separates
     )
 
 
@@ -385,34 +388,3 @@ def _verify_loss_args(
         raise ValueError(
             f"The prediction array should be at least two dimensional if the first dimension separates the samples. The current shape of the prediction array: {tuple(pred.shape)}"
         )
-
-
-def _calculate_masked_mean_unchecked(
-    values: torch.Tensor,
-    mask: torch.Tensor,
-    first_dim_separates: bool = False,
-) -> torch.Tensor:
-    """
-    A function that calculates the masked mean of the given values.
-
-    This function does not check its arguments.
-
-    Parameters
-    ----------
-    values
-        The values of which the mean should be calculated. Format: the array should contain floating data. If ``first_dim_separates``, then it should have at least two dimensions. It should have the same shape as the predicted values.
-    mask
-        The masks that select the relevant values. Format: the array should contain boolean data. If ``first_dim_separates``, then it should have at least two dimensions. It should have the same shape as the predicted values.
-    first_dim_separates
-        If this is true, then the mean calculation is done for each element along dimension 0 individually. Otherwise the claculation is done for the whole array globally.
-    """
-
-    if first_dim_separates:
-        dim = tuple(range(1, len(values.shape)))
-    else:
-        dim = None
-
-    values = values * mask
-    return values.mean(dim=dim) * (
-        torch.ones_like(values).sum(dim=dim) / mask.to(values.dtype).sum(dim=dim)
-    )

test/test_losses.py
@@ -135,6 +135,8 @@ class TestLosses(TestBase):
         )
         self.assertAllclose(actual_d001_losses, self.expected_d001_losses)
         self.assertAllclose(actual_d100_losses, self.expected_d100_losses)
+        self.assertEqual(len(actual_d001_losses.shape), 1)
+        self.assertEqual(len(actual_d100_losses.shape), 1)
 
     def test_dx_loss__np__not_enough_dimensions(self) -> None:
         with self.assertRaises(ValueError) as cm:
@@ -175,6 +177,8 @@ class TestLosses(TestBase):
         )
         self.assertAllclose(actual_d001_losses, self.expected_d001_losses[0])
         self.assertAllclose(actual_d100_losses, self.expected_d100_losses[0])
+        self.assertEqual(len(actual_d001_losses.shape), 1)
+        self.assertEqual(len(actual_d100_losses.shape), 1)
 
     def test_dx_loss__pt__happy_path(self) -> None:
         with torch.no_grad():
@@ -197,6 +201,8 @@ class TestLosses(TestBase):
 
         self.assertAllclose(actual_d001_losses, self.expected_d001_losses)
         self.assertAllclose(actual_d100_losses, self.expected_d100_losses)
+        self.assertEqual(len(actual_d001_losses.shape), 1)
+        self.assertEqual(len(actual_d100_losses.shape), 1)
 
     def test_dx_loss__np__invalid_arrays(self):
         def fn(pred, gt, mask):
@@ -221,12 +227,14 @@ class TestLosses(TestBase):
             pred=self.pred, gt=self.gt, mask=self.mask, first_dim_separates=True
         )
         self.assertAllclose(actual_mse_losses, self.expected_mse_losses)
+        self.assertEqual(len(actual_mse_losses.shape), 1)
 
     def test_mse_loss__np__happy_path__single_value(self):
         actual_mse_losses = depth_tools.mse_loss(
             pred=self.pred[0], gt=self.gt[0], mask=self.mask[0]
         )
         self.assertAllclose(actual_mse_losses, self.expected_mse_losses[0])
+        self.assertEqual(len(actual_mse_losses.shape), 1)
 
     def test_mse_loss__pt__happy_path(self):
         with torch.no_grad():
@@ -238,6 +246,7 @@ class TestLosses(TestBase):
                 verify_args=True,
             ).numpy()
         self.assertAllclose(actual_mse_losses, self.expected_mse_losses)
+        self.assertEqual(len(actual_mse_losses.shape), 1)
 
     def test_mse_loss__np__not_enough_dimensions(self) -> None:
         with self.assertRaises(ValueError) as cm:
@@ -291,6 +300,7 @@ class TestLosses(TestBase):
             pred=self.pred, gt=self.gt, mask=self.mask, first_dim_separates=True
         )
         self.assertAllclose(actual_mse_log_losses, self.expected_mse_log_losses)
+        self.assertEqual(len(actual_mse_log_losses.shape), 1)
 
     def test_mse_log_loss__pt__happy_path(self):
         with torch.no_grad():
@@ -302,6 +312,7 @@ class TestLosses(TestBase):
                 verify_args=True,
             ).numpy()
         self.assertAllclose(actual_mse_log_losses, self.expected_mse_log_losses)
+        self.assertEqual(len(actual_mse_log_losses.shape), 1)
 
     def test_mse_log_loss__np__not_enough_dimensions(self) -> None:
         with self.assertRaises(ValueError) as cm:
@@ -348,72 +359,6 @@ class TestLosses(TestBase):
                 )
 
         self.probe_invalid_inputs([self.pred, self.gt, self.mask], fn)
-
-    def test_calculate_masked_mean__np__nosep(self):
-        arr = np.full((30, 40), 0.1, dtype=np.float32)
-        mask = np.full((30, 40), True, dtype=np.bool_)
-
-        arr[:, 1] = 10000
-        mask[:, 1] = False
-
-        actual_mean = cast(
-            float,
-            depth_tools._losses_univ._calculate_masked_mean_unchecked(
-                values=arr, mask=mask, first_dim_separates=False
-            ).item(),
-        )
-        expected_mean = 0.1
-
-        self.assertAlmostEqual(actual_mean, expected_mean)
-
-    def test_calculate_masked_mean__np__sep(self):
-        arr = np.full((30, 40), 0.1, dtype=np.float32)
-        mask = np.full((30, 40), True, dtype=np.bool_)
-
-        arr[:, 1] = 10000
-        mask[:, 1] = False
-
-        actual_mean = depth_tools._losses_univ._calculate_masked_mean_unchecked(
-            values=arr, mask=mask, first_dim_separates=True
-        )
-        expected_mean = np.full((30), 0.1, dtype=np.float32)
-
-        self.assertAllclose(actual_mean, expected_mean)
-
-    def test_calculate_masked_mean__pt__nosep(self):
-        arr = np.full((30, 40), 0.1, dtype=np.float32)
-        mask = np.full((30, 40), True, dtype=np.bool_)
-
-        arr[:, 1] = 10000
-        mask[:, 1] = False
-
-        actual_mean = cast(
-            float,
-            depth_tools.pt._losses_univ._calculate_masked_mean_unchecked(
-                values=torch.from_numpy(arr),
-                mask=torch.from_numpy(mask),
-                first_dim_separates=False,
-            ).item(),
-        )
-        expected_mean = 0.1
-
-        self.assertAlmostEqual(actual_mean, expected_mean)
-
-    def test_calculate_masked_mean__pt__sep(self):
-        arr = np.full((30, 40), 0.1, dtype=np.float32)
-        mask = np.full((30, 40), True, dtype=np.bool_)
-
-        arr[:, 1] = 10000
-        mask[:, 1] = False
-
-        actual_mean = depth_tools.pt._losses_univ._calculate_masked_mean_unchecked(
-            values=torch.from_numpy(arr),
-            mask=torch.from_numpy(mask),
-            first_dim_separates=True,
-        ).numpy()
-        expected_mean = np.full((30), 0.1, dtype=np.float32)
-
-        self.assertAllclose(actual_mean, expected_mean)
 
 
 class TestEvalBuilder(TestBase):